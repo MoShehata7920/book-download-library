889507cf77f59075cc8f6dfec33dededebbee279
moalnawagy <nawagymohamed@gmail.com>
2022-03-29 00:01:19 +0200

    First commit

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,31 @@
 const express = require('express');
 
-const notes = require("./data/notes");
 const dotenv = require('dotenv');
-
+const connectDB = require('./config/db')
+const userRoutes = require('./routes/userRoutes')
 const app = express();
+
+
+
 const port = process.env.PORT || 5000;
 dotenv.config();
+connectDB()
+
 
 app.get('/', (req, res) => {
     res.send("API is working now");
 
 });
-
+app.use('/api/users', userRoutes)
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    res.json({});
 
 });
-app.get('/api/notes/:id', (req, res) => {
-    const note = notes.find((n) => n._id === req.params.id);
-    res.send(note)
+// app.get('/api/notes/:id', (req, res) => {
+//     const note = notes.find((n) => n._id === req.params.id);
+//     res.send(note)
 
-});
+// });
 app.listen(port, () => {
     console.log(`server is working on port: ${port}`)
 });
