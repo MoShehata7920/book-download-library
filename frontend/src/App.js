510ed4c5a109bc93a860f50bd7e4427d5a2b3b5c
MoShehata7920 <mshehata065@gmail.com>
2022-04-07 06:27:15 +0200

    frontend editing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <main style={{minHeight:"93vh"}}>
+
+    </main>
+    <Footer />
+  </>
+)
 
 export default App;
